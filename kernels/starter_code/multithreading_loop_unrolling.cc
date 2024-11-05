9d0ecccd4ebd96399200bf590decc0025b2d3b92
HJiang <33575549+Huanhuan-Jiang@users.noreply.github.com>
2024-11-05 18:33:04 +0800

    Finish multithreading_loop_unrolling (#4)

--- kernels/starter_code/multithreading_loop_unrolling.cc
+++ kernels/starter_code/multithreading_loop_unrolling.cc
@@ -58,6 +58,15 @@ static void *multithreading_loop_unrolling_worker_func(void *args) {
                 for (int qj = 0; qj < 16; qj++) {
                     // TODO: decode a packed byte into two int8 in the range of (-8, 7)
 
+                    intermediate_sum0 += a_int8[qj] * ((w0_int4[qj] & 0x0F) - 8.0);
+                    intermediate_sum0 += a_int8[qj + 16] * ((w0_int4[qj] >> 4) - 8.0);
+                    intermediate_sum1 += a_int8[qj] * ((w1_int4[qj] & 0x0F) - 8.0);
+                    intermediate_sum1 += a_int8[qj + 16] * ((w1_int4[qj] >> 4) - 8.0);
+                    intermediate_sum2 += a_int8[qj] * ((w2_int4[qj] & 0x0F) - 8.0);
+                    intermediate_sum2 += a_int8[qj + 16] * ((w2_int4[qj] >> 4) - 8.0);
+                    intermediate_sum3 += a_int8[qj] * ((w3_int4[qj] & 0x0F) - 8.0);
+                    intermediate_sum3 += a_int8[qj + 16] * ((w3_int4[qj] >> 4) - 8.0);
+
                     // TODO: int8 multiply and accumulate operation
                 }
                 // dequantize the sum into floating point
@@ -90,6 +99,14 @@ static void *multithreading_loop_unrolling_worker_func(void *args) {
                     intermediate_sum3_2nd = 0;
                 for (int qj = 0; qj < 32; qj++) {
                     // TODO: decode a packed byte into two int8 in the range of (-8, 7)
+                    intermediate_sum0 += a_int8[qj] * ((w0_int4[qj] & 0x0F) - 8.0);
+                    intermediate_sum0_2nd += a_int8[qj + 32] * ((w0_int4[qj] >> 4) - 8.0);
+                    intermediate_sum1 += a_int8[qj] * ((w1_int4[qj] & 0x0F) - 8.0);
+                    intermediate_sum1_2nd += a_int8[qj + 32] * ((w1_int4[qj] >> 4) - 8.0);
+                    intermediate_sum2 += a_int8[qj] * ((w2_int4[qj] & 0x0F) - 8.0);
+                    intermediate_sum2_2nd += a_int8[qj + 32] * ((w2_int4[qj] >> 4) - 8.0);
+                    intermediate_sum3 += a_int8[qj] * ((w3_int4[qj] & 0x0F) - 8.0);
+                    intermediate_sum3_2nd += a_int8[qj + 32] * ((w3_int4[qj] >> 4) - 8.0);
 
                     // TODO: int8 multiply and accumulate operation
                 }
@@ -131,7 +148,16 @@ void MatmulOperator::mat_mul_multithreading_loop_unrolling(struct matmul_params 
     assert(params->block_size == 32);  // support block size 32 for now
 
     // TODO: Thread creation
+    for (auto i = 0; i < num_thread; i++) {
+        threads_args[i].start = i * (n / num_thread);
+        threads_args[i].end = (i + 1) * (n / num_thread);
+        threads_args[i].params = params;
+        pthread_create(&thread_pool[i], nullptr, multithreading_loop_unrolling_worker_func, &threads_args[i]);
+    }
 
     // TODO: Join threads
+    for (int i = 0; i < num_thread; i++) {
+        pthread_join(thread_pool[i], nullptr);
+    }
 };
 }  // namespace matmul
